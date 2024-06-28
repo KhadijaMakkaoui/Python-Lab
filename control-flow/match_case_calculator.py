7ab52ec0a01501105cb110c26d326e1da2f8c932
Khadija Makkaoui <makkaoui.khadija44@gmail.com>
2024-06-28 10:23:13 +0100

    correct answer

--- control-flow/match_case_calculator.py
+++ control-flow/match_case_calculator.py
@@ -3,13 +3,13 @@ num2=int(input("Enter the second number:"))
 operation=input("Choose the operation (+, -, *, /):")
 match operation:
     case '+':
-        input("The result is :"+str(num1+num2))
+        input("The result is "+str(num1+num2))
     case '-':
-        input("The result is :"+str(num1-num2))
+        input("The result is "+str(num1-num2))
     case '*':
-        input("The result is :"+str(num1*num2))
+        input("The result is "+str(num1*num2))
     case '/':
         if(num2==0):
             input("Cannot divide by zero.")
         else:
-            input("The result is :"+str(num1/num2))
+            input("The result is "+str(num1/num2))
