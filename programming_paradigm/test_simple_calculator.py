ea2857b01fed964d0d3a444ec880eb7b995074f5
Khadija Makkaoui <makkaoui.khadija44@gmail.com>
2024-08-05 16:51:12 +0100

    correct answer

--- programming_paradigm/test_simple_calculator.py
+++ programming_paradigm/test_simple_calculator.py
@@ -4,23 +4,19 @@ from simple_calculator import SimpleCalculator
 class Test(unittest.TestCase):
 
     def setUp(self):
-        self.calculator= SimpleCalculator()    
+        self.calc= SimpleCalculator()    
     
-    def test_add(self):
-        result= self.calculator.add(2, 3)
-        self.assertEqual(result,5)
+    def test_addition(self):
+        self.assertEqual(self.calc.add(2, 3),5)
 
-    def test_add(self):
-        result= self.calculator.subtract(2, 3)
-        self.assertEqual(result,-1)
+    def test_subtraction(self):
+        self.assertEqual(self.calc.subtract(2, 3),-1)
     
     def test_multiply(self):
-        result= self.calculator.multiply(2, 3)
-        self.assertEqual(result,6)
+        self.assertEqual(self.calc.multiply(2, 3),6)
     
     def test_divide(self):
-        result= self.calculator.divide(4, 2)
-        self.assertEqual(result,2)
+        self.assertEqual(self.calc.divide(4, 2),2)
         
 
         
