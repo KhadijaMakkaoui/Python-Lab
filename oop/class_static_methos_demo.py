2bab9e8fc1f26d92dd3aed9e6b602865b34e3866
Khadija Makkaoui <makkaoui.khadija44@gmail.com>
2024-08-05 09:23:01 +0100

    add main

--- oop/class_static_methos_demo.py
+++ oop/class_static_methos_demo.py
@@ -9,4 +9,16 @@ class Calculator:
     def multiply(cls,a,b):
         print(cls.calculation_type)
         return a * b
-    
+
+
+def main():
+    # Using the static method
+    sum_result = Calculator.add(10, 5)
+    print(f"The sum is: {sum_result}")
+
+    # Using the class method
+    product_result = Calculator.multiply(10, 5)
+    print(f"The product is: {product_result}")
+
+if __name__ == "__main__":
+    main()
