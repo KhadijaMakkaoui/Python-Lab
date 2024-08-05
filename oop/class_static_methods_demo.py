8c810850782a836d14808bbf90272d21bb94829c
Khadija Makkaoui <makkaoui.khadija44@gmail.com>
2024-08-05 09:28:36 +0100

    add space

--- oop/class_static_methods_demo.py
+++ oop/class_static_methods_demo.py
@@ -2,11 +2,11 @@ class Calculator:
     calculation_type= "Arithmetic Operations"
 
     @staticmethod
-    def add(a,b):
+    def add(a, b):
         return a + b
     
     @classmethod
-    def multiply(cls,a,b):
+    def multiply(cls, a, b):
         print("Calculation type: {cls.calculation_type}")
         return a * b
     
